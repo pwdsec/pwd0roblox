5adec769a601b3c2ebf6ef4042af3b15f3c4d8fa
pwd0kernel <pwd0kernel@gmail.com>
2021-11-05 16:00:18 -0230

    Update rbx_installer.go

--- roblox/rbx_installer.go
+++ roblox/rbx_installer.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"pwd0roblox/console"
 )
 
 // deletes roblox from pc.
@@ -22,14 +23,25 @@ func DeleteRoblox() {
 // downloads roblox and installs it.
 func InstallRoblox(version string, start bool) {
 	println("	[+] Downloading Roblox...")
-	resp, err := http.Get("https://setup.rbxcdn.com/" + version + "-Roblox.exe")
+
+	var url string
+	var file_name string
+	if console.IsWindows() {
+		url = "https://setup.rbxcdn.com/" + version + "-Roblox.exe"
+		file_name = "Roblox.exe"
+	} else if console.IsMacOS() {
+		url = "http://setup.roblox.com/mac/" + version + "-Roblox.dmg"
+		file_name = "Roblox.dmg"
+	}
+
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
 	// write file
-	file, err := os.Create("Roblox.exe")
+	file, err := os.Create(file_name)
 	if err != nil {
 		panic(err)
 	}
@@ -44,48 +56,16 @@ func InstallRoblox(version string, start bool) {
 	println("	[+] Roblox downloaded!")
 	if start {
 		println("	[+] Installing Roblox...")
-		// run file
-		cmd := exec.Command("Roblox.exe")
-		cmd.Start()
-		cmd.Wait()
-
+		if console.IsWindows() {
+			cmd := exec.Command("Roblox.exe")
+			cmd.Start()
+			cmd.Wait()
+		} else if console.IsMacOS() {
+			cmd := exec.Command("hdiutil", "attach", "Roblox.dmg")
+			cmd.Start()
+			cmd.Wait()
+		}
 		println("	[+] Roblox installed!")
-
-		// delete the installer
-		os.Remove("Roblox.exe")
-	}
-}
-
-func InstallRobloxMac(version string, start bool) {
-	println("	[+] Downloading Roblox...")
-
-	resp, err := http.Get("http://setup.roblox.com/mac/" + version + "-Roblox.dmg")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	// write file
-	file, err := os.Create("Roblox.dmg")
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	file.Close()
-
-	println("	[+] Roblox downloaded!")
-
-	if start {
-		println("	[+] Installing Roblox...")
-		// install dmg file
-		cmd := exec.Command("hdiutil", "attach", "Roblox.dmg")
-		cmd.Start()
-		cmd.Wait()
-
-		println("	[+] Roblox installed!")
+		os.Remove(file_name)
 	}
 }

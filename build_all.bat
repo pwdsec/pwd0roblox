056ad75625745a59f0264de155f8958017b10e80
pwd0kernel <pwd0kernel@gmail.com>
2021-11-14 20:07:13 -0330

    Create build_all.bat

---
+++ build_all.bat
@@ -0,0 +1,6 @@
+go env -w GOOS=windows GOARCH=386
+go build -o bin/win/x86/pwd0roblox_x86.exe
+go env -w GOOS=windows GOARCH=amd64
+go build -o bin/win/x64/pwd0roblox_x64.exe
+go env -w GOOS=darwin GOARCH=amd64
+go build -o bin/mac/pwd0roblox

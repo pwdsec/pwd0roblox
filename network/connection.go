6400e59ba73e663e2e158eeaf090af92a401d696
pwd0kernel <pwd0kernel@gmail.com>
2021-11-17 22:25:36 -0330

    Update connection.go

--- network/connection.go
+++ network/connection.go
@@ -2,7 +2,6 @@ package network
 
 import "net"
 
-// check if user is connected to internet using net
 func IsConnected() bool {
 	if _, err := net.Dial("tcp", "google.com:80"); err != nil {
 		return false

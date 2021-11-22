0bc80410b9dd022c9b5ea8418dc855085c001f74
pwd0kernel <pwd0kernel@gmail.com>
2021-11-21 23:24:07 -0330

    update

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,5 @@
-rsrc -ico pwd0robloxLogo.ico
+rsrc -ico pwd0robloxLogo.ico -arch amd64
+rsrc -ico pwd0robloxLogo.ico -arch 386
 go env -w GOOS=windows GOARCH=386
 go build -o bin/win/x86/pwd0roblox_x86.exe
 go env -w GOOS=windows GOARCH=amd64

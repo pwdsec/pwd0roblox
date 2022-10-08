502d21b0c3ce29b44025ee0368ad4eb57166192f
pwd0kernel <79817991+pwd0kernel@users.noreply.github.com>
2022-10-08 17:36:48 -0230

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+# check if golang is installed else ask to install
+if ! [ -x "$(command -v go)" ]; then
+  echo 'Error: golang is not installed.' >&2
+  echo 'Please install golang and try again.' >&2
+  exit 1
+fi
+
 if [ "$(uname)" == "Darwin" ]; then
     if [ -d "bin/macos" ]; then
         echo "Deleting old build..."
